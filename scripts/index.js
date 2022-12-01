924443ea22dce342c369783d2c3b43064973edaf
U41HA <lp.lead-box@yandex.ru>
2022-12-01 16:06:37 +0500

    feat IIFE initApp function

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,21 @@
-import {showMonth} from './functions/_showMonth.js';
-import {quickAddPopupShow} from './functions/_quickAddPopupShow.js';
-import {addOrOverviewEvent} from './functions/_addOrOverviewEvent.js';
-import {searchPopupShow} from './functions/_searchPopupShow.js';
-import {setLocaleStorageSearchList} from './functions/_setLocaleStorageSearchList.js';
-import {getLocaleStorageSearchList} from './functions/_getLocaleStorageSearchList.js';
-import {setActiveDay} from './functions/_setActiveDay.js';
+import {showMonth} from './functions/showMonth.js';
+import {quickAddPopupShow} from './functions/quickAddPopupShow.js';
+import {addOrOverviewEvent} from './functions/addOrOverviewEvent.js';
+import {searchPopupShow} from './functions/searchPopupShow.js';
+import {setLocaleStorageSearchList} from './functions/setLocaleStorageSearchList.js';
+import {getLocaleStorageSearchList} from './functions/getLocaleStorageSearchList.js';
+import {setActiveDay} from './functions/setActiveDay.js';
 
-const daysCell = document.querySelectorAll('.day');
-const addEventButton = document.querySelector('.buttons__add');
-const searchInput = document.querySelector('.search__input');
-
-window.addEventListener('load', showMonth);
-daysCell.forEach(day => day.addEventListener('click', setActiveDay));
-daysCell.forEach(day => day.addEventListener('click', addOrOverviewEvent));
-addEventButton.addEventListener('click', quickAddPopupShow);
-searchInput.addEventListener('click', searchPopupShow);
-window.addEventListener('beforeunload', setLocaleStorageSearchList);
-window.addEventListener('load', getLocaleStorageSearchList);
+(function initApp() {
+    const daysCell = document.querySelectorAll('.day');
+    const addEventButton = document.querySelector('.buttons__add');
+    const searchInput = document.querySelector('.search__input');
+    
+    window.addEventListener('load', showMonth);
+    daysCell.forEach(day => day.addEventListener('click', setActiveDay));
+    daysCell.forEach(day => day.addEventListener('click', addOrOverviewEvent));
+    addEventButton.addEventListener('click', quickAddPopupShow);
+    searchInput.addEventListener('click', searchPopupShow);
+    window.addEventListener('beforeunload', setLocaleStorageSearchList);
+    window.addEventListener('load', getLocaleStorageSearchList);
+})();
